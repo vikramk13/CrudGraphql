24655dde64befdd0901d0ae8d20f6a15c1c61bdd
AL3013 <AL3013@ALIPL1147.accionlabs.in>
2020-09-04 17:35:33 +0530

    data is sent through postman

--- Hibernate_OneToMany/src/main/java/com/company/HibernateOneToManyApplication.java
+++ Hibernate_OneToMany/src/main/java/com/company/HibernateOneToManyApplication.java
@@ -14,55 +14,50 @@ import com.company.repo.UserAdressRepo;
 import com.company.repo.UserPersonRepo;
 
 @SpringBootApplication
-public class HibernateOneToManyApplication implements CommandLineRunner {
+public class HibernateOneToManyApplication /* implements CommandLineRunner */ {
 
 	public static void main(String[] args) {
 		SpringApplication.run(HibernateOneToManyApplication.class, args);
 	}
 	
-	@Autowired
-	private  UserPersonRepo repo;
-	
-	
-	@Autowired
-	private UserAdressRepo repo1;
-	
-	
-
-	@Override
-	public void run(String... args) throws Exception {
-		// TODO Auto-generated method stub
-		
-		
-		
-		
-		
-		UserPerson u=new UserPerson();
-		
-		
-	
-		u.setFirstName("vijay");
-		u.setLastName("raaghava");
-	
-		Address a=new Address();
-		
-		  a.setPlace("Bangalore"); 
-		  a.setStreet("Marathahalli");
-		  
-		  Address a1=new Address();
-		  a1.setPlace("Hyderabad");
-		  a1.setStreet("kphb");
-		 
-		
-	List<Address>	adre=  Arrays.asList(a,a1);
-	
-
-		u.setAdress(adre);
-		
-	repo.save(u);
-		
-		
-		
-	}
+	/*
+	 * @Autowired private UserPersonRepo repo;
+	 * 
+	 * 
+	 * @Autowired private UserAdressRepo repo1;
+	 * 
+	 * 
+	 * 
+	 * @Override public void run(String... args) throws Exception { // TODO
+	 * Auto-generated method stub
+	 * 
+	 * 
+	 * 
+	 * 
+	 * 
+	 * UserPerson u=new UserPerson();
+	 * 
+	 * 
+	 * 
+	 * u.setFirstName("vijay"); u.setLastName("raaghava");
+	 * 
+	 * Address a=new Address();
+	 * 
+	 * a.setPlace("Bangalore"); a.setStreet("Marathahalli");
+	 * 
+	 * Address a1=new Address(); a1.setPlace("Hyderabad"); a1.setStreet("kphb");
+	 * 
+	 * 
+	 * List<Address> adre= Arrays.asList(a,a1);
+	 * 
+	 * 
+	 * u.setAdress(adre);
+	 * 
+	 * repo.save(u);
+	 * 
+	 * 
+	 * 
+	 * }
+	 */
 
 }
--- Hibernate_OneToMany/src/main/java/com/company/controller/UserPersonController.java
+++ Hibernate_OneToMany/src/main/java/com/company/controller/UserPersonController.java
@@ -5,10 +5,13 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.company.model.Address;
+import com.company.model.UserPerson;
 import com.company.repo.UserAdressRepo;
 import com.company.repo.UserPersonRepo;
 
@@ -20,8 +23,23 @@ public class UserPersonController {
 	@Autowired
 	private UserPersonRepo userrepo;
 	
+	
+	
+	
+	
+	
 	@Autowired
 	private UserAdressRepo repo1;
+	
+	
+	@PostMapping("createuser")
+	public UserPerson create(@RequestBody UserPerson user)
+	{
+		
+	return 	userrepo.save(user);
+	}
+	
+	
 	
 	@GetMapping("getUserInfo/{id}")
 	public Optional<com.company.model.UserPerson> getById(@PathVariable Integer id)
